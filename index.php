e88f5f5e4797acf76fbba02e2e1cbc3c2a39e0c2
Darren Wehmeyer <darweh0808@gmail.com>
2019-02-16 13:18:42 -0800

    Add files via upload
    new php file using php includes
    and no doctype. send only
    meta tags for screen, location css,

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/head.ini';?>
+<img class="cover_pic" src="/application/image/cover.jpg" />
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/cover_header.ini';?>
